058b7d47a667e466d149a2965a0f3d39c2f6354d
octocat <155513340+Matias2710@users.noreply.github.com>
2025-02-14 17:47:47 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirCarta() {
+    const sobre = document.querySelector('.sobre');
+    const mensaje = document.getElementById('mensaje');
+
+    sobre.style.transform = 'translateY(-100%)';
+    mensaje.style.top = '0';
+}
