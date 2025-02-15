47a752ec9798f8a6da747122a386d50043416c04
octocat <155513340+Matias2710@users.noreply.github.com>
2025-02-14 18:24:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ function abrirCarta() {
     sobre.style.transform = 'translateY(-100%)';
     mensaje.style.top = '0';
 }
+
+function crearLluviaCorazones() {
+    const lluvia = document.createElement("div");
+    lluvia.classList.add("lluvia");
+    document.body.appendChild(lluvia);
+
+    setInterval(() => {
+        const corazon = document.createElement("div");
+        corazon.classList.add("corazon-chico");
+        corazon.innerHTML = "❤️";
+        corazon.style.left = Math.random() * 100 + "vw";
+        corazon.style.animationDuration = (Math.random() * 2 + 3) + "s";
+
+        lluvia.appendChild(corazon);
+
+        setTimeout(() => {
+            corazon.remove();
+        }, 5000);
+    }, 300);
+}
+
+crearLluviaCorazones();
